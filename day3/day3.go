a5950fb840c497ea63fc407b89cf3b0e6215d03e
Rxfa <rafa3rnrn@gmail.com>
2025-12-07 04:12:31 +0000

    solve day 3

---
+++ day3/day3.go
@@ -0,0 +1,56 @@
+package day3
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func maxDigit(s string) (byte, int) {
+	maxNum := byte('0')
+	maxNumIdx := -1
+	for i := 0; i < len(s); i++ {
+		if s[i] > maxNum {
+			maxNum = s[i]
+			maxNumIdx = i
+		}
+	}
+	return maxNum, maxNumIdx
+}
+
+func getMaxJoltage(bank string) int {
+	a, aIdx := maxDigit(bank)
+	var b byte
+
+	if aIdx == len(bank)-1 {
+		b, _ = maxDigit(bank[:aIdx])
+	} else {
+		b, _ = maxDigit(bank[aIdx+1:])
+	}
+	return int(a-'0')*10 + int(b-'0')
+}
+
+func lobby(filepath string) (int, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	total := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		total += getMaxJoltage(scanner.Text())
+	}
+	return total, nil
+}
+
+func Day3() {
+	filepath := "day3/input.txt"
+	solution, err := lobby(filepath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("[day 3] The solution is: %d\n", solution)
+}
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/rxfa/aoc-2025/day1"
 	"github.com/rxfa/aoc-2025/day2"
+	"github.com/rxfa/aoc-2025/day3"
 )
 
 func main() {
 	day1.Day1()
 	day2.Day2()
+	day3.Day3()
 }
